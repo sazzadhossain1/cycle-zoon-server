331ca239af72f5aba854fc584aea6877ab877997
sazzadhossain1 <sazzadtomal1@gmail.com>
2023-07-19 14:36:13 +0600

    create a new api

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ app.use(cors());
 
 const cycleData = require("./data/cycle.json");
 const newsData = require("./data/news.json");
+const accessoriesData = require("./data/accessories.json");
 
 app.get("/", (req, res) => {
   res.send("Cycle zoon server is running");
@@ -23,6 +24,8 @@ app.get("/cycleData/:id", (req, res) => {
   res.send(cycles);
 });
 
+// ====================== //
+
 app.get("/newsData", (req, res) => {
   res.send(newsData);
 });
@@ -32,6 +35,12 @@ app.get("/newsData/:id", (req, res) => {
   const newsVar = newsData.find((news) => news.id === id);
   res.send(newsVar);
 });
+
+// ================= //
+app.get("/accessoriesData", (req, res) => {
+  res.send(accessoriesData);
+});
+
 app.listen(port, () => {
   console.log("Cycle zoon port is running");
 });
